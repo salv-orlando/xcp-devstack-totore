bf52ce37a37ce24d3404a6e11290ff1c41b194fe
Mike McClurg <mike.mcclurg@citrix.com>
2012-01-16 13:26:34 +0000

    Tidy up prepare_dom0.sh
    Only need to apt-get git, not vim or git build reqs. Also make DEVSTACK var
    assignment more succinct.
    
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

--- tools/xcp-toolstack/prepare_dom0.sh
+++ tools/xcp-toolstack/prepare_dom0.sh
@@ -3,12 +3,10 @@ set -o xtrace
 set -o errexit
 
 # Install basics for vi and git
-apt-get install gcc make vim zlib1g-dev libssl-dev git
+apt-get install git
 
 # Clone devstack
-if [ -z ${DEVSTACK} ]; then
-    DEVSTACK=/root/devstack
-fi
+DEVSTACK=${DEVSTACK:-/root/devstack}
 
 if [ ! -d ${DEVSTACK} ]; then
     git clone git://github.com/cloudbuilders/devstack.git ${DEVSTACK}
